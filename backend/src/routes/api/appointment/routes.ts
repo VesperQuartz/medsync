faa794df488e400106def49d342c3731c8299fd2
mrlectus <lilbrown3000@gmail.com>
2025-03-09 16:42:44 +0100

    fix 2

--- backend/src/routes/api/appointment/routes.ts
+++ backend/src/routes/api/appointment/routes.ts
@@ -1,37 +1,13 @@
 import {
   AppointmentInsertSchema,
   AppointmentSelectSchema,
+  StaffSelectSchema,
   UserSelectSchema,
 } from "@/shared/schema.js";
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
 
 const appointments: FastifyPluginAsyncZod = async function (fastify, _opts) {
-  fastify.route({
-    method: "POST",
-    url: "/",
-    schema: {
-      body: AppointmentInsertSchema.pick({
-        doctorId: true,
-        patientId: true,
-        reason: true,
-        duration: true,
-      }),
-      response: {
-        200: AppointmentSelectSchema,
-      },
-    },
-    handler: async (req, _res) => {
-      const { doctorId, patientId, reason, duration } = req.body;
-      const appointment = await fastify.BookAppointment.execute({
-        doctorId,
-        patientId,
-        reason,
-        duration,
-      });
-      return appointment;
-    },
-  });
   fastify.route({
     method: "POST",
     url: "/",
@@ -65,8 +41,30 @@ const appointments: FastifyPluginAsyncZod = async function (fastify, _opts) {
         200: z.array(AppointmentSelectSchema),
       },
     },
-    handler: async (req, _res) => {
+    handler: async (_req, _res) => {
       const appointment = await fastify.AllAppointment.execute();
+      return appointment;
+    },
+  });
+  fastify.route({
+    method: "GET",
+    url: "/:id",
+    schema: {
+      params: z.object({
+        id: z.coerce.number(),
+      }),
+      response: {
+        200: z.array(
+          z.object({
+            appointments: AppointmentSelectSchema.or(z.null()),
+            staff: StaffSelectSchema.or(z.null()),
+          }),
+        ),
+      },
+    },
+    handler: async (req, _res) => {
+      const { id } = req.params;
+      const appointment = await fastify.ShowAppointment.execute(id);
       return appointment;
     },
   });
