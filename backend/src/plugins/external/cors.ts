79d8b3299d2c51d98fa13260692805a0db1b15f6
mrlectus <lilbrown3000@gmail.com>
2025-03-11 00:09:50 +0100

    fixed cors

--- backend/src/plugins/external/cors.ts
+++ backend/src/plugins/external/cors.ts
@@ -5,9 +5,10 @@ export default fp(async (fastify) => {
   await fastify.register(cors, {
     credentials: true,
     origin: [
-    /http(s)?:\/\/localhost:([0-9]+)?/,
-    /http(s)?:\/\/127.0.0.1:([0-9]+)?/,
+      /http(s)?:\/\/localhost:([0-9]+)?/,
+      /http(s)?:\/\/127.0.0.1:([0-9]+)?/,
       "https://mrlectus.local",
+      "https://medsync-pi.vercel.app/",
     ],
     exposedHeaders: ["Authorization", "Set-Cookie"],
   });
